82205b094d58c8645098e81333c900f8199d2092
Ted Singer <github@fernmyth.net>
2024-07-23 23:19:45 -0400

    f

--- kappa/settings.py
+++ kappa/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'default-insecure-key-for-dev')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', "onrender.com"]  # Add your domain here
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', "*.onrender.com"]  # Add your domain here
 
 
 # Application definition
