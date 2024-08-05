7ee9735cad8a88382d8ca0042a29b1ad5813d27e
Ted Singer <github@fernmyth.net>
2024-08-05 00:20:05 -0400

    rename. cleanup

--- schema_dump.sql
+++ schema_dump.sql
@@ -18,5 +18,21 @@ CREATE TABLE public.messages (
     body text
 );
 ALTER TABLE public.messages OWNER TO twilio;
+CREATE TABLE public.purchase_offers (
+    unique_id uuid NOT NULL,
+    tel character varying(20) NOT NULL,
+    offer_date timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
+ALTER TABLE public.purchase_offers OWNER TO twilio;
+CREATE TABLE public.purchases (
+    tel character varying(20),
+    purchase_date timestamp without time zone,
+    purchase_type character varying(50),
+    message_count integer
+);
+ALTER TABLE public.purchases OWNER TO twilio;
+ALTER TABLE ONLY public.purchase_offers
+    ADD CONSTRAINT purchase_offers_pkey PRIMARY KEY (unique_id);
 CREATE INDEX messages_tel ON public.messages USING btree (tel);
 ALTER TABLE public.messages CLUSTER ON messages_tel;
+CREATE INDEX purchases_tel ON public.purchases USING btree (tel);
--- smartypants/create_purchases_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE public.purchases (
-    tel character varying(20),
-    purchase_date timestamp without time zone,
-    purchase_type character varying(50),
-    message_count integer
-);
-
-ALTER TABLE public.purchases OWNER TO twilio;
-CREATE INDEX purchases_tel ON public.purchases USING btree (tel);
